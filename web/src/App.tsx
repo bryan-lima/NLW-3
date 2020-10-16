0a3d866cf969b21cf4f3167171e8cdd4267aec3c
Bryan <bryanlima200@outlook.com>
2020-10-16 00:57:54 -0300

    Add imagens

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+  <h1>{props.text}</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title />
     </div>
   );
 }

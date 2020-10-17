84f5a1a6b93586962fb1377622b6a0388e2ac95c
Bryan <bryanlima200@outlook.com>
2020-10-17 16:34:34 -0300

    Alterado API de mapa para Mapbox Para verificar estilos de mapa do Mapbox, acesse: https://docs.mapbox.com/mapbox-gl-js/api/map/

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -31,7 +31,10 @@ function OrphanagesMap() {
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
             </Map>
 
             <Link to="" className="create-orphanage">

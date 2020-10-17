72380a3f267715dfab34769a79e4ed272c4bdb11
Bryan <bryanlima200@outlook.com>
2020-10-17 15:46:29 -0300

    Criado página com mapa usando API Leaflet Veja mais sobre esta API em: https://leafletjs.com/

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Jacareí</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-23.2883422,-45.9705647]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
     );
 }
 

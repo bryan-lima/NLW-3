4adc938a0f922c9952b22afe7cab588196e7b133
Bryan <bryanlima200@outlook.com>
2020-10-17 14:35:54 -0300

    Criado a landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
-interface TitleProps {
-  text: string;
-}
+import './styles/global.css';
+import './styles/pages/landing.css';
 
-function Title(props: TitleProps) {
-  return (
-  <h1>{props.text}</h1>
-  )
-}
+import LogoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <Title />
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={LogoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Jacareí</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="/" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }

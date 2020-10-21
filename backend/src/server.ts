8ceac75791c56d03baa2dfe1b42611348550e1fa
Bryan <bryanlima200@outlook.com>
2020-10-21 18:56:23 -0300

    Add biblioteca typeorm e banco de dados SQLite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    return response.json({ message : 'Hello World'});
+});
+
 app.listen(3333);

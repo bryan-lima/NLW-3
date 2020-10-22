e33a676f5a3435e8304fc8afae08200008bebb3e
Bryan <bryanlima200@outlook.com>
2020-10-22 20:56:42 -0300

    Add marcador na página de mapa e transição entre telas

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -35,9 +44,21 @@ function OrphanagesMap() {
                 <TileLayer 
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-23.2883422,-45.9705647]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="orphanages/1">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
             </Link>
         </div>

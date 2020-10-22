c1012a6c1ab8f977cd697fd5392372e63acbe5e5
Bryan <bryanlima200@outlook.com>
2020-10-22 15:05:33 -0300

    Add módulo cors para permitir requisições de outros domínios Requisições do front-end, por exemplo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -9,6 +11,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));

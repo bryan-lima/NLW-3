acefacae5d02ca03185ea6268bdd046f69ab6f07
Bryan <bryanlima200@outlook.com>
2020-10-22 14:11:20 -0300

    Add views

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
+
 import './database/connection';
+
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);

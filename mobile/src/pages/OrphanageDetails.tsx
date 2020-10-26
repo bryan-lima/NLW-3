d6c6420013957559f1d23e199e3430347508a728
Bryan <bryanlima200@outlook.com>
2020-10-25 21:25:15 -0300

    Finalizado listagem de orfanatos (consumindo API) na página de mapa

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
 
 import mapMarkerImg from '../images/map-marker.png';
-import { RectButton } from 'react-native-gesture-handler';
 
 export default function OrphanageDetails() {
+  const route = useRoute();
+
+  console.log(route.params);
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
